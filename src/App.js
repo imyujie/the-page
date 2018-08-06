0954741d06890be34d5380da42c993180f9ec2ed
yujie <im_yujie@foxmail.com>
2018-08-06 10:02:21 +0800

    beta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,101 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import chatdata from './data.json'
+import './App.css'
 import { CSSTransitionGroup } from 'react-transition-group'
 
-
 class Bubble extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isTyping: false,
+      width: 'auto',
+      height: 'auto'
+    }
+    this.shadowBubble = React.createRef()
+  }
+  componentWillMount() {
+    const sender = this.props.sender
 
+    if (this.props.content.indexOf('<img') >= 0) {
+      var res = this.props.content.match(/src="([^"]+)"/)
+      this.setState({
+        isTyping: true,
+        width: '64px',
+        height: '40px'
+      })
+
+      var img = new Image()
+      img.onload = _ => {
+        this.setState({
+          width: this.shadowBubble.current.offsetWidth,
+          height: this.shadowBubble.current.offsetHeight
+        })
+        setTimeout(_ => {
+          this.setState({isTyping: false})
+          this.props.onFinish()
+        }, 250)
+      }
+
+      img.src = res[1]
+      
+    } else {
+      const length = this.props.content.length
+      if (sender == 'me' && length > 20) {
+        this.setState({
+          isTyping: true,
+          width: '64px',
+          height: '40px'
+        })
+        setTimeout(_ => {
+          this.setState({
+            width: this.shadowBubble.current.offsetWidth,
+            height: this.shadowBubble.current.offsetHeight
+          })
+          setTimeout(_ => {
+            this.setState({isTyping: false})
+            this.props.onFinish()
+          }, 250)
+        }, length / 20 * 600)
+      }
+    }
+    
+    
+  }
   render() {
+    var b
+
+    var c
+
+    if (this.props.content.indexOf('<img') >= 0) {
+      c = <div className="img-content" dangerouslySetInnerHTML={{__html: this.props.content}}></div>
+    } else {
+      c = <div className="content" dangerouslySetInnerHTML={{__html: this.props.content}}></div>
+    }
+
+    if (this.state.isTyping) {
+      b = (
+      <div className="loader">
+        <div className="wave">
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+        </div>
+      </div>)
+    } else {
+      b = c 
+    }
+
     return (
-      <div className="chat-row">
-        
-        <div className={this.props.sender == 'me' ? 'bubble left' : 'bubble right'}>
-          {this.props.content}
+      <div className={this.props.sender == 'me' ? 'row left-row' : 'row right-row'}>
+        <div className="chat-row">
+          <div style={{width: this.state.width, height: this.state.height}} className={this.props.sender == 'me' ? 'bubble left' : 'bubble right'}>
+            {b}
+          </div>
+        </div>
+        <div className="shadow chat-row">
+          <div ref={this.shadowBubble} className={this.props.sender == 'me' ? 'mock bubble left' : 'mock bubble right'}>
+            {c}
+          </div>
         </div>
       </div>
     )
@@ -22,19 +106,12 @@ class Bubble extends Component {
 class Replies extends Component {
   onReplyClick(answer, goto) {
     return () => {
-      console.log('click')
       this.props.onRClick(answer, goto)
     }
   }
   render() {
     return (
-      <CSSTransitionGroup
-          component="div"
-          className="bottom-container"
-          transitionName="fade"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={500}>
-        <div className="inner" key={this.props.akey}>
+        <div className={this.props.isSendingMsg ? 'disabled inner' : 'inner'} key={this.props.akey}>
             <div className="replies">
               {this.props.replies.map((r, i) =>
                 <button onClick={this.onReplyClick(r.answer, r.goto)}>{r.answer}</button>
@@ -42,7 +119,6 @@ class Replies extends Component {
               <div className="blank"></div>
             </div>
         </div>
-      </CSSTransitionGroup>
     )
   }
 }
@@ -50,42 +126,9 @@ class Replies extends Component {
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      chatPool: {
-        "ice": {
-          "says": [
-            "你好",
-            "Hello",
-          ],
-          "reply": [
-            {
-              "answer": "我现在不想和你说话，好烦",
-              "goto": "apple"
-            },
-            {
-              "answer": "你在说什么呀",
-              "goto": "apple"
-            },
-            {
-              "answer": "我记不太清楚了",
-              "goto": "apple"
-            }
-          ]
-        },
-        "apple": {
-          "says": [
-            "苹果",
-            "Hello",
-          ],
-          "reply": [
-            {
-              "answer": "Em",
-              "goto": "ice"
-            }
-          ]
-        },
-      },
+      chatPool: chatdata,
       msgList: [
         // {
         //   "sender": "",
@@ -98,20 +141,19 @@ class App extends Component {
         //   "goto": ""
         // }
       ],
+      nextKey: 'ice',
       sender: 'me',
-      repId: 0
-    };
+      repId: 0,
+      isSendingMessage: true,
+      buffer: []
+    }
     this.scrollList = React.createRef()
-  }
-
-  componentWillAppear() {
-    console.log('appear')
   }
 
   delay(amount = 0) {
     return new Promise(resolve => {
-        setTimeout(resolve, amount);
-    });
+      setTimeout(resolve, amount)
+    })
   }
 
   say(sender, content) {
@@ -121,86 +163,107 @@ class App extends Component {
     this.setState(prevState => ({
       msgList: [...prevState.msgList, { sender, content } ]
     }))
-    this.updateScroll()
+    this.delay(100).then(this.updateScroll.bind(this))
   }
 
   updateScroll() {
     const $chatbox = this.scrollList.current
-    const distance = $chatbox.scrollHeight - $chatbox.offsetHeight - $chatbox.scrollTop;
-    const duration = 250;
-    const startTime = Date.now();
+    const distance = $chatbox.scrollHeight - $chatbox.offsetHeight - $chatbox.scrollTop
+    const duration = 250
+    const startTime = Date.now()
     requestAnimationFrame(function step() {
-        const p = Math.min(1, (Date.now() - startTime) / duration);
+        const p = Math.min(1, (Date.now() - startTime) / duration)
         $chatbox.scrollTop = $chatbox.scrollTop + distance * p
-        p < 1 && requestAnimationFrame(step);
-    });
+        if (p < 1) {
+          requestAnimationFrame(step)
+        } else {
+          return
+        }
+    })
   }
 
-  giveReplies(replies) {
+  updateReplies() {
+    var nextKey = this.state.nextKey
+    var replies = this.state.chatPool[nextKey].reply
     this.setState({ repId: this.time() })
     this.setState({ replies })
+    this.delay(0).then(_ => this.setState({ isSendingMessage: false }))
   }
 
-  getReplies(k = 'ice') {
-    var chat = this.state.chatPool[k]
-    return chat.reply
+
+  getMyWords() {
+    var nextKey = this.state.nextKey
+    var chat = [...this.state.chatPool[nextKey].says]
+    this.setState({buffer: chat.reverse()})
   }
 
-  getMyWords(k = 'ice') {
-    var chat = this.state.chatPool[k]
-    return [...chat.says.reverse()]
-  }
-
-  getOneMsg(msg) {
-    if (msg.length == 0) {
+  getOneMsg() {
+    if (this.state.buffer.length == 0) {
       return false
     }
-    return msg.pop()
+    var bufferCopy = [...this.state.buffer]
+    var result = bufferCopy.pop()
+    this.setState({buffer: [...bufferCopy]})
+    return result
+  }
+
+  getAndSay() {
+    var content = this.getOneMsg()
+    if (content) {
+      this.say(this.state.sender, content)
+    } else {
+      this.updateReplies()
+      this.setState({
+        isSendingMessage: false
+      }) 
+    }
+    
   }
 
   componentDidMount() {
+    this.setState({
+      nextKey: 'ice'
+    })
+    this.getMyWords()
+    this.updateReplies()
+    this.delay(0).then(_ => this.setState({ isSendingMessage: true }))
+    setTimeout(_ => {
+      this.getAndSay()
+    }, 200)
+  }
 
-    var words = this.getMyWords()
-    var aa = _ => {
-      var c = this.getOneMsg(words)
-      if (c) {
-        this.say('me', c)
-        setTimeout(aa, 500)
-      } else {
-        return
-      }
-    }
+  onSent() {
+    this.delay(0).then(this.updateScroll.bind(this))
+    this.delay(500).then(_ => {
+      this.getAndSay()
+    })
 
-    aa()
-
-    this.giveReplies(this.getReplies())
-    
   }
 
   handleReplyClick(answer, goto) {
-    this.say('you', answer)
-    var words = this.getMyWords(goto)
-
-    var pp = new Promise(resolve => {
-      var aa = _ => {
-        var c = this.getOneMsg(words)
-        if (c) {
-          this.say('me', c)
-          setTimeout(aa, 500)
-        } else {
-          resolve()
-          return
-        }
-      }
-  
-      aa()
-    })
+    if (this.state.isSendingMessage) return
     
-    
-    pp.then(_ => {
-      this.giveReplies(this.getReplies(goto))
+    this.setState({
+      isSendingMessage: true,
+      sender: 'you',
+      buffer: [answer]
     })
 
+    this.delay(200).then(_ => {
+      this.getAndSay()
+      this.setState({
+        nextKey: goto
+      })
+      this.getMyWords()
+      this.delay(500).then(_ => {
+        this.setState({
+          sender: 'me'
+        })
+        this.getAndSay()
+      })
+    })
+
+    
   }
 
   time() {
@@ -218,17 +281,17 @@ class App extends Component {
           transitionEnterTimeout={500} 
           transitionLeaveTimeout={500}>
             {this.state.msgList.map((m) =>
-              <Bubble sender={m['sender']} content={m['content']} />
+              <Bubble onFinish={this.onSent.bind(this)} sender={m['sender']} content={m['content']} />
             )}
           </CSSTransitionGroup>
         </div>
 
         <div className="quick-replies scrollx">
-            <Replies akey={this.state.repId} replies={this.state.replies} onRClick={this.handleReplyClick.bind(this)}/>
+            <Replies isSendingMsg={this.state.isSendingMessage} akey={this.state.repId} replies={this.state.replies} onRClick={this.handleReplyClick.bind(this)}/>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
