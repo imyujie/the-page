3d2f3439129f1e3e189ab34ae1105da8e1502b94
yujie <im_yujie@foxmail.com>
2018-08-09 13:52:11 +0800

    v12

--- src/App.js
+++ src/App.js
@@ -17,14 +17,15 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const MESSAGE_GAP = 700
+const MESSAGE_GAP = 400
 const SENDER_SWITCH_GAP = 800
 const START_AFTER = 1000
 const SHORT_GAP = 100
 const IMAGE_LOADING_PADDING = 500
 const RESIZING_TIME = 250
+const REPLY_TRANSITION_TIME = 300
 const SCROLL_DURATION = 300
-const MINIMUM_TYPING_TIME = 500
+const MINIMUM_TYPING_TIME = 450
 const MSG_ANIMATE_TIME = 500
 
 class Bubble extends Component {
@@ -32,44 +33,38 @@ class Bubble extends Component {
     super(props)
     this.state = {
       isTyping: false,
-      width: 'auto',
-      height: 'auto'
+      width: '0',
+      height: '0'
     }
     this.shadowBubble = React.createRef()
   }
 
   removeLoadingAfter(dl = 0) {
+    return delay(dl).then(_ => this.setStateAsync({isTyping: false}))
+  }
+
+  setStateAsync(newState) {
     return new Promise(resolve => {
-      delay(dl).then(_ => {
-        this.setState({isTyping: false})
-        delay(0).then(resolve.bind(this))
-      })
+      this.setState(newState, _ => resolve())
     })
-    
   }
 
   addLoading() {
-    return new Promise(resolve => {
-      this.setState({
-        isTyping: true,
-        width: '64px',
-        height: '40px'
-      })
-      delay(0).then(resolve.bind(this))
+    return this.setStateAsync({
+      isTyping: true,
+      width: '64px',
+      height: '40px'
     })
-    
   }
 
   updateSizeAfter(dl = 0) {
-    return new Promise(resolve => {
-      delay(dl).then(_ => {
-        this.setState({
-          width: this.shadowBubble.current.offsetWidth,
-          height: this.shadowBubble.current.offsetHeight
-        })
-        delay(0).then(resolve.bind(this))
+    return delay(dl)
+    .then(_ =>
+      this.setStateAsync({
+        width: this.shadowBubble.current.offsetWidth,
+        height: this.shadowBubble.current.offsetHeight
       })
-    })
+    )
   }
 
 
@@ -80,18 +75,18 @@ class Bubble extends Component {
       return
     }
 
-    if (content.indexOf('<img') >= 0 || content.length >= 10) {
-      this.addLoading()
-    }
+    this.addLoading()
 
   }
 
   componentDidMount() {
     const { isTyping } = this.state
-    const { sender, content, wait } = this.props
+    const { content, wait } = this.props
 
     if (isTyping) {
       this.props.onHeightChange()
+
+      // for image message
       if (content.indexOf('<img') >= 0) {
         var res = content.match(/src="([^"]+)"/)
         var img = new Image()
@@ -101,18 +96,19 @@ class Bubble extends Component {
           .then(_ => this.props.onFinish())
         }
         img.src = res[1]
-      } else if (content.length >= 10) {
-        var typingTime = MINIMUM_TYPING_TIME + content.length / 10 * 400
-        this.addLoading().then(_ => {
-          this.updateSizeAfter(typingTime + wait)
+
+      // for long text
+      } else {
+        var typingTime = content.length >= 10 ? MINIMUM_TYPING_TIME + content.length / 10 * 400 : MINIMUM_TYPING_TIME
+        this.updateSizeAfter(typingTime + wait)
           .then(_ => this.removeLoadingAfter(RESIZING_TIME))
           .then(_ => this.props.onFinish())
-        })
       }
     } else {
-      if (sender == 'me') {
-        delay(0).then(_ => this.props.onFinish())
-      }
+      this.setStateAsync({
+        width: this.shadowBubble.current.offsetWidth,
+        height: this.shadowBubble.current.offsetHeight
+      }).then(_ => this.props.onFinish())
     }
   }
 
@@ -129,20 +125,28 @@ class Bubble extends Component {
       </div>) : c
 
     return (
-      <div className={this.props.sender === 'me' ? 'row left-row' : 'row right-row'}>
-       
-        <div className="chat-row">
-          <div style={{width: this.state.width, height: this.state.height}} className={this.props.sender === 'me' ? 'bubble left' : 'bubble right'}>
-            {b}
+      <CSSTransitionGroup
+          component="div" 
+          className="animate-row-container"
+          transitionName="bounceIn" 
+          transitionAppear={true}
+          transitionEnter={false}
+          transitionLeave={false}
+          transitionAppearTimeout={MSG_ANIMATE_TIME} >
+        <div className={this.props.sender === 'me' ? 'row left-row' : 'row right-row'}>
+          <div className="chat-row">
+            <div style={{width: this.state.width, height: this.state.height}} className={this.props.sender === 'me' ? 'bubble bubble-transition left' : 'bubble right'}>
+              {b}
+            </div>
           </div>
-        </div>
 
-        <div className="shadow chat-row">
-          <div ref={this.shadowBubble} className={this.props.sender === 'me' ? 'mock bubble left' : 'mock bubble right'}>
-            {c}
+          <div className="shadow chat-row">
+            <div ref={this.shadowBubble} className={this.props.sender === 'me' ? 'mock bubble left' : 'mock bubble right'}>
+              {c}
+            </div>
           </div>
         </div>
-      </div>
+      </CSSTransitionGroup>
     )
 
   }
@@ -161,8 +165,8 @@ class Replies extends Component {
           component="div"
           className="inner-container"
           transitionName="fade" 
-          transitionEnterTimeout={300} 
-          transitionLeaveTimeout={300}>
+          transitionEnterTimeout={REPLY_TRANSITION_TIME} 
+          transitionLeaveTimeout={REPLY_TRANSITION_TIME}>
           <div className="replies" key={this.props.akey}>
             {this.props.replies.map((r, i) =>
               <button key={i} onClick={this.onReplyClick(r.answer, r.goto)}>{r.answer}</button>
@@ -192,19 +196,16 @@ class App extends Component {
     this.scrollList = React.createRef()
   }
 
-  say(sender, content, wait) {
-    this.setState(prevState => ({
-      sender,
-      msgList: [...prevState.msgList, { sender, content, wait } ]
-    }))
-    // delay(SHORT_GAP).then(this.updateScroll.bind(this))
-  }
-
   updateScroll() {
     return new Promise(resolve => {
       const $chatbox = this.scrollList.current
       const distance = $chatbox.scrollHeight - $chatbox.offsetHeight - $chatbox.scrollTop
       const startTime = Date.now()
+
+      if (distance === 0) {
+        resolve()
+        return
+      }
       requestAnimationFrame(function step() {
         const p = Math.min(1, (Date.now() - startTime) / SCROLL_DURATION)
         $chatbox.scrollTop = $chatbox.scrollTop + distance * p
@@ -218,8 +219,9 @@ class App extends Component {
     const {nextKey, chatPool} = this.state
     var replies = chatPool[nextKey].reply
     
-    this.setState({ repId: time(), replies })
-    delay(SHORT_GAP).then(_ => this.setState({ isSendingMessage: false }))
+    this.setStateAsync({ repId: time(), replies })
+    .then(_ => delay(REPLY_TRANSITION_TIME))
+    .then(_ => this.setStateAsync({ isSendingMessage: false }))
   }
 
 
@@ -236,7 +238,8 @@ class App extends Component {
       })
       return v
     })
-    this.setState({ buffer: result.reverse() })
+
+    return result.reverse()
   }
 
   getOneMsg() {
@@ -250,64 +253,69 @@ class App extends Component {
   }
 
   getAndSay() {
-    var msg = this.getOneMsg()
+    const msg = this.getOneMsg()
 
-    if (msg) {
-      this.setState({ isSendingMessage: true})
-      const { content, wait } = msg
-      this.say(this.state.sender, content, wait)
-    } else {
-      this.updateReplies()
-    }
+    const { content, wait } = msg
+    const { sender } = this.state
     
+    this.setState(prevState => ({
+      isSendingMessage: true,
+      sender,
+      msgList: [...prevState.msgList, { sender, content, wait } ]
+    }))
+
   }
 
   componentWillMount() {
     this.setState({ nextKey: 'ice' })
-    this.getMyWords()
-    this.updateReplies()
   }
 
   componentDidMount() {
-    delay(SHORT_GAP).then(_ => this.getAndSay())
-  }
-
-  onHeightChange() {
-    this.updateScroll()
+    this.sendMessage('me', this.getMyWords())
   }
 
   onSent() {
     if (this.state.buffer.length === 0) {
-      this.updateReplies()
+      if (this.state.sender === 'me') {
+        this.updateReplies()
+      }
       this.updateScroll()
       return
     }
     this.updateScroll().then(_ => 
-      delay(getRandomNumber(0, 300)).then(_ => this.getAndSay())
+      delay(getRandomNumber(0, MESSAGE_GAP)).then(_ => this.getAndSay())
     )
+  }
+
+  setStateAsync(newState) {
+    return new Promise(resolve => {
+      this.setState(newState, _ => resolve())
+    })
+  }
+
+  sendMessage(sender, msgs) {
+    return new Promise(resolve => {
+      const newState = {
+        isSendingMessage: true,
+        sender,
+        buffer: msgs
+      }
+
+      this.setStateAsync(newState).then(_ => {
+        this.getAndSay()
+        resolve()
+      })
+    })
   }
 
   handleReplyClick(answer, goto) {
     if (this.state.isSendingMessage) return
     
-    this.setState({
-      isSendingMessage: true,
-      sender: 'you',
-      buffer: [ {content: answer, wait: 0} ]
-    })
-
-    delay(0).then(_ => {
-      this.getAndSay()
-      this.updateScroll()
-
-      this.setState({ nextKey: goto })
-      this.getMyWords()
-
-      delay(SENDER_SWITCH_GAP).then(_ => {
-        this.setState({ sender: 'me' })
-        this.getAndSay()
-      })
-    })
+    this.sendMessage('you', [ {content: answer, wait: 0} ])
+    .then(_ => this.setStateAsync({ nextKey: goto }))
+    .then(_ => delay(SENDER_SWITCH_GAP))
+    .then(_ => this.setStateAsync({ sender: 'me' }))
+    .then(_ => this.sendMessage('me', this.getMyWords()))
   }
 
 
@@ -315,15 +323,10 @@ class App extends Component {
     return (
       <div className="app">
         <div className="message-list scrolly" ref={this.scrollList}>
-          <CSSTransitionGroup
-          component="div" 
-          transitionName="bounceIn" 
-          transitionEnterTimeout={MSG_ANIMATE_TIME} 
-          transitionLeaveTimeout={MSG_ANIMATE_TIME}>
-            {this.state.msgList.map((m, i) =>
-              <Bubble key={i} onFinish={this.onSent.bind(this)} onHeightChange={this.onHeightChange.bind(this)} {...m} />
-            )}
-          </CSSTransitionGroup>
+          {this.state.msgList.map((m, i) =>
+            <Bubble key={i} onFinish={this.onSent.bind(this)} onHeightChange={this.updateScroll.bind(this)} {...m} />
+          )}
+          <div className="msg-list-padding"></div>
         </div>
 
         <div className="quick-replies scrollx">
